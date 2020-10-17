81341b5e659a240d778e144c015ec7e55ccf70be
Sañyam Kumar <sanyamkvvs@hotmail.com>
2020-10-17 20:18:15 +0530

    v 0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rxjs-fun';
+
+  constructor() {
+    const observable = Observable.create((observer) => {
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+      observer.complete();
+    });
+    observable.subscribe(
+      value => console.log(value),
+      err => {},
+      () => console.log('All Done!')
+    );
+  }
+
 }
